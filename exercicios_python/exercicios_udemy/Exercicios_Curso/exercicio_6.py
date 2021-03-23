be381fd96dd635bec44b983bad360faf3e4f2587
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-03-23 03:13:00 -0300

    Resolvendo exercicio 6

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_6.py
@@ -0,0 +1,15 @@
+'''
+Faça um programa que calcule a raiz quadrada de
+um número. O usuário deve inserir um número e o
+programa deve mostrar na tela o resultado da
+raiz quadrada do número inserido.
+'''
+
+#Modo 1
+numero = int(input("Digite um numero: "))
+print(f"A raiz quadrada de {numero} é {numero**0.5}")
+
+#modo 2
+from math import sqrt
+
+print(f"A raiz quadrada de {numero} é {sqrt(numero)}")
