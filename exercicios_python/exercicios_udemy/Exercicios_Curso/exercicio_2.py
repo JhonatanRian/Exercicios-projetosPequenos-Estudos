2d75947219fe0293309f25cf2d6eefb0bc98d71f
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-03-23 02:03:23 -0300

    exercicios 1 e 2 feito

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_2.py
@@ -0,0 +1,16 @@
+'''Faça um programa que solicite um número do usuário e apresente a seguinte mensagem na tela:
+
+O numero digitado foi ...'''
+
+#primeiro modo
+numero = input("Digite um numero: ")
+print(f"O numero digitado foi {numero}")
+
+#Segundo modo(observação, pode gerar um ValueError)
+numero = int(input("Digite um numero: "))
+print(f"O numero digitado foi {numero}")
+
+#terceiro modo(observação, pode gerar um ValueError)
+numero = float(input("Digite um numero: "))
+print(f"O numero digitado foi {numero}")
+
