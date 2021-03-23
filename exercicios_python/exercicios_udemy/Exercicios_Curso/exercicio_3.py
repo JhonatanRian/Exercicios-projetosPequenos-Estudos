6afb9fc92baa9f6a4866768b71f37d7664d76d95
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-03-23 02:24:26 -0300

    exercicio 3 concluido

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_3.py
@@ -0,0 +1,9 @@
+'''
+Escreva um programa que solicite o nome
+sobrenome do usuário. Ao final o programa
+deverá apresentar o nome completo do usuário na
+tela.
+'''
+nome = str(input("Qual seu primeiro nome?\n》 "))
+sobrenome = str(input("Qual seu sobrenome?\n》"))
+print(f"Seu nome é {nome} {sobrenome}")
