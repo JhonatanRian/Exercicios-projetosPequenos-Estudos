231aec81cc9eb2239508b8e7befb6fe92c48dd89
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-03-23 02:46:18 -0300

    Exercicio 4 e 5 feito

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_4.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que solicite três números
+inteiros do usuário e imprima a soma destes
+'''
+
+n1 = int(input("digite um numero: "))
+n2 = int(input("digite outro numero: "))
+n3 = int(input("digite mais um numero: "))
+print(f"{n1} + {n2} + {n3} = {n1+n2+n3}")
