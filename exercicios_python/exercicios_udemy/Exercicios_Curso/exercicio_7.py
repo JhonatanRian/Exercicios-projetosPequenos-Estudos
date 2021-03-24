fd807e7165601f9deb2144330560446aa0401755
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-03-24 03:04:16 -0300

    Aqui foi adicionado os exercicios sete, oito e nove

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_7.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que peça 5 números de ponto
+flutuante do usuário e apresente no final a
+média dos números digitados.
+'''
+
+n1 = float(input("numero: "))
+n2 = float(input("numero: "))
+n3 = float(input("numero: "))
+n4 = float(input("numero: "))
+n5 = float(input("numero: "))
+media = n1 + n2 + n3 + n5 +n5 / 5 
+
+print(f"A média é {media}")
---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_8.py
@@ -0,0 +1,6 @@
+'''
+Escreva um programa que faça a conversão de um
+dado valor de metro para quilômetro.
+'''
+n = float(input("num: "))
+print(f'{n} em metros é {n/1000}')
