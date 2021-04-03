0797bcd7c96dab4803df4eb9d3328406d668862d
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-04-02 22:59:36 -0300

    Exercicio_20 e exercicio 21 feito

---
+++ exercicios_python/exercicios_udemy/Exercicios_Curso/exercicio_20(TABUADA).py
@@ -0,0 +1,10 @@
+'''
+Escreva um programa que peça um número inteiro do usuário e calcule e imprima a tabuada deste número.
+'''
+n = int(input('Informe o numero que deseja ver a tabuada: '))
+for i in range(11):
+    multi = f'{i:2}x{n:<2} = {i*n:<3} |'
+    soma = f'{i:2}+{n:<2} = {i+n:<3} |'
+    sub = f'{i:2}-{n:<2} = {i-n:<3} |'
+    div = f'{i:2}/{n:<2} = {i/n:.2f} |'
+    print(multi, soma, div, sub)
