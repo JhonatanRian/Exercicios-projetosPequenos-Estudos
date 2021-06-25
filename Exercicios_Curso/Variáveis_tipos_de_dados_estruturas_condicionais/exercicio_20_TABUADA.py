d5548b6730000f345c3c75c64e6b814020c6452f
Jhonatan Rian <64628254+JhonatanRian@users.noreply.github.com>
2021-06-25 17:48:27 -0300

    Update exercicio_20_TABUADA.py

--- Exercicios_Curso/Variáveis_tipos_de_dados_estruturas_condicionais/exercicio_20_TABUADA.py
+++ Exercicios_Curso/Variáveis_tipos_de_dados_estruturas_condicionais/exercicio_20_TABUADA.py
@@ -7,8 +7,8 @@
 n = int(input('Informe o numero que deseja ver a tabuada: '))
 #  Retorna a tabuada usando 'for' e formatação de strings
 for i in range(11):
-    multi = f'{i:2}x{n:<2} = {i*n:<3} |'
-    soma = f'{i:2}+{n:<2} = {i+n:<3} |'
-    sub = f'{i:2}-{n:<2} = {i-n:<3} |'
-    div = f'{i:2}/{n:<2} = {i/n:.2f} |'
+    multi = f'{i:2}x{numero:<2} = {i*numero:<3} |'
+    soma = f'{i:2}+{numero:<2} = {i+numero:<3} |'
+    sub = f'{i:2}-{numero:<2} = {i-numero:<3} |'
+    div = f'{i:2}/{numero:<2} = {i/numero:.2f} |'
     print(multi, soma, div, sub)
